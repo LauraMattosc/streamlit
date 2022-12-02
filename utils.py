11ec3ebd5265636920a9d349d8ce41edf9158580
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2022-12-02 06:46:58 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -153,7 +153,7 @@ def build_graph_v2(df):
                
 
     config = Config(width='100%', 
-                    height=3000,
+                    height=1000,
                     collapsible = True,
                     overlap = False,
                     nodesep = 100,
